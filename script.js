c0dfabf372a0ccfd5e2cbf9b66d712e98a11d049
Fouzia ifat <105037033+Fouziaifat@users.noreply.github.com>
2023-09-04 02:21:50 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.Starbuck').src = anything;
+}
+function changeCircleColor(color){
+    let circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
